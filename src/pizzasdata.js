5b2af319b9c3ae20a416bedf6515f7cb2aa1463b
amarc <amarchuadhary547@gmail.com>
2023-09-03 12:14:21 +0530

    first commit

---
+++ src/pizzasdata.js
@@ -0,0 +1,111 @@
+const pizzas = [
+    {
+        "name": "MARGHERITA",
+        "varients": [
+            "small",
+            "medium",
+            "large"
+        ],
+        "prices": [
+            {
+                "small": 200,
+                "medium": 350,
+                "large": 400
+            }
+        ],
+        "category": "veg",
+        "image": "https://www.dominos.co.in/files/items/Margherit.jpg",
+        "description": "A hugely popular margherita, with a deliciously tangy single cheese topping"
+    },
+    {
+        "name": "PEPPY PANEER",
+        "varients": [
+            "small",
+            "medium",
+            "large"
+        ],
+        "prices": [
+            {
+                "small": 200,
+                "medium": 350,
+                "large": 400
+            }
+        ],
+        "category": "veg",
+        "image": "https://www.dominos.co.in/files/items/Peppy_Paneer.jpg",
+        "description": "Chunky paneer with crisp capsicum and spicy red pepper - quite a mouthful"
+    },
+    {
+        "name": "FARM HOUSE",
+        "varients": [
+            "small",
+            "medium",
+            "large"
+        ],
+        "prices": [
+            {
+                "small": 200,
+                "medium": 350,
+                "large": 400
+            }
+        ],
+        "category": "veg",
+        "image": "https://www.dominos.co.in/files/items/Farmhouse.jpg",
+        "description": "A pizza that goes ballistic on veggies! Check out this mouth watering overload of crunchy, crisp capsicum, succulent mushrooms and fresh tomatoes"
+    },
+    {
+        "name": "PEPPER BARBECUE CHICKEN",
+        "varients": [
+            "small",
+            "medium",
+            "large"
+        ],
+        "prices": [
+            {
+                "small": 350,
+                "medium": 450,
+                "large": 550
+            }
+        ],
+        "category": "nonveg",
+        "image": "https://www.dominos.co.in/files/items/MicrosoftTeams-image_(15).png",
+        "description": "Pepper Barbecue Chicken I Cheese"
+    },
+    {
+        "name": "CHICKEN GOLDEN DELIGHT",
+        "varients": [
+            "small",
+            "medium",
+            "large"
+        ],
+        "prices": [
+            {
+                "small": 350,
+                "medium": 450,
+                "large": 550
+            }
+        ],
+        "category": "nonveg",
+        "image": "https://www.dominos.co.in/files/items/MicrosoftTeams-image_(14).png",
+        "description": "Mmm! Barbeque chicken with a topping of golden corn loaded with extra cheese. Worth its weight in gold!"
+    },
+    {
+        "name": "CHICKEN DOMINATOR",
+        "varients": [
+            "small",
+            "medium",
+            "large"
+        ],
+        "prices": [
+            {
+                "small": 350,
+                "medium": 450,
+                "large": 550
+            }
+        ],
+        "category": "nonveg",
+        "image": "https://www.dominos.co.in/files/items/MicrosoftTeams-image_(11).png",
+        "description": "Treat your taste buds with Double Pepper Barbecue Chicken, Peri-Peri Chicken, Chicken Tikka &amp; Grilled Chicken Rashers"
+    }
+]
+export default pizzas;
